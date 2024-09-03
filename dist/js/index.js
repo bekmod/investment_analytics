ebee47cde81cc3adb864de174fe43dd85f972b06
Kiryl <kirill.budnik@hotmail.com>
2024-09-03 17:45:23 +0300

    chore: Update images and add pop-up window for videos

--- dist/js/index.js
+++ dist/js/index.js
@@ -13,3 +13,31 @@ tabs.forEach((tab, index) => {
         tabContent[index].classList.add('active');
     });
 });
+
+// Cache DOM elements
+const overlay = document.getElementById('overlay');
+const popup = document.getElementById('popup');
+const iframe = document.getElementById('video-iframe');
+const closePopupButton = document.getElementById('close-popup');
+
+// Event delegation for video links
+document.addEventListener('click', function (e) {
+    if (e.target.classList.contains('video-link')) {
+        e.preventDefault();
+        const videoId = e.target.getAttribute('data-video');
+        iframe.src = `https://www.youtube.com/embed/${videoId}?modestbranding=1&rel=0&showinfo=0&controls=1`;
+        overlay.style.display = 'block';
+        popup.style.display = 'block';
+    }
+});
+
+// Close modal function
+function closeModal() {
+    overlay.style.display = 'none';
+    popup.style.display = 'none';
+    iframe.src = ''; // Clear the src attribute to stop video
+}
+
+// Event listeners for closing the modal
+closePopupButton.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
